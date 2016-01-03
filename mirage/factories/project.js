ed3a1e04e3108326baf88f9657c687ad5d95dab1
Hugo Ruscitti <hugoruscitti@gmail.com>
2016-01-03 17:43:14 -0300

    Creando atributo de código inicial y una vista para ejecutar.

--- mirage/factories/project.js
+++ mirage/factories/project.js
@@ -4,4 +4,19 @@ export default Mirage.Factory.extend({
   title: function(i) {
      return 'project ' + i + ' ' + faker.name.firstName();
   },
+  initialCode: `
+  var canvas = document.getElementById('canvas'),
+      context = canvas.getContext('2d');
+
+  context.font = '38pt Arial';
+  context.fillStyle = 'cornflowerblue';
+  context.strokeStyle = 'blue';
+
+  context.fillText("Hello Canvas", canvas.width/2 - 150,
+                                   canvas.height/2 + 15);
+
+  context.strokeText("Hello Canvas", canvas.width/2 - 150,
+                                     canvas.height/2 + 15 );
+                                     
+  `
 });
