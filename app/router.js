8fd98a7be96242a0d595b90f9644c6b347b8631d
Hugo Ruscitti <hugoruscitti@gmail.com>
2016-01-04 12:18:48 -0300

    Creando modal para la sección "acerca de..."

--- app/router.js
+++ app/router.js
@@ -9,6 +9,10 @@ Router.map(function() {
   this.route('project');
   this.route('run', {path: "run/:project_id"});
   this.route('edit', {path: "edit/:project_id"});
+
+  this.route('index', {path: "/"}, function() {
+    this.route('about');
+  });
 });
 
 export default Router;
