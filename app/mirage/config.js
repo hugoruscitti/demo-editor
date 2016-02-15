46a581ef31004a9b6543a460d6f33ee7d693a677
Hugo Ruscitti <hugoruscitti@gmail.com>
2016-02-15 15:34:43 -0300

    Arreglo menor para que mirage funcione en electro.

--- app/mirage/config.js
+++ app/mirage/config.js
@@ -1,5 +1,5 @@
 export default function() {
-
+  this.urlPrefix = '/';    // make this `http://localhost:8080`, for example, if your API is on a different server
   this.get('/projects');
   this.get('/projects/:id');
   this.put('/projects/:id');
@@ -11,7 +11,6 @@ export default function() {
 
     Note: these only affect routes defined *after* them!
   */
-  // this.urlPrefix = '';    // make this `http://localhost:8080`, for example, if your API is on a different server
   // this.namespace = '';    // make this `api`, for example, if your API is namespaced
   // this.timing = 400;      // delay for each request, automatically set to 0 during testing
 
