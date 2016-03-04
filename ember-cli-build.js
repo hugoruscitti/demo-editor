1ef30b08f6b547f0358884b3f314be6ae691fe8c
Hugo Ruscitti <hugoruscitti@gmail.com>
2016-03-04 16:04:07 -0300

    update

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -31,5 +31,21 @@ module.exports = function(defaults) {
   app.import('vendor/transpiler.js');
 
 
+  /* Necesarios para ember-cli-jsconsole */
+  app.import("./bower_components/codemirror/lib/codemirror.css");
+  app.import("./bower_components/codemirror/theme/eclipse.css");
+  app.import("./bower_components/codemirror/lib/codemirror.js");
+  app.import("./bower_components/codemirror/addon/hint/javascript-hint.js");
+  app.import("./bower_components/codemirror/addon/hint/show-hint.css");
+  app.import("./bower_components/codemirror/addon/hint/show-hint.js");
+ 
+  app.import("./bower_components/codemirror/mode/javascript/javascript.js");
+ 
+  app.import("./bower_components/jsconsole/styles/console.css");
+  app.import("./bower_components/jsconsole/dist/console.js");
+  app.import("./bower_components/jsconsole/styles/gutter-icons.png", {destDir: 'assets/'});
+  app.import("./bower_components/jsconsole/styles/meslo/MesloLGSDZ-Regular.woff", {destDir: 'assets/meslo'});
+  /* FIN: Necesarios para ember-cli-jsconsole */
+
   return app.toTree();
 };
