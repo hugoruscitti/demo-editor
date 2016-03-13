9939cd371a3055c0ddd1b8cbc5d9b6ba0b066a00
Hugo Ruscitti <hugoruscitti@gmail.com>
2016-03-13 20:20:47 -0300

    update

--- app/controllers/edit.js
+++ app/controllers/edit.js
@@ -5,6 +5,7 @@ export default Ember.Controller.extend({
   rightPanelVisible: true,
   largePreview: false,
   queryParams: ['leftPanelVisible', 'rightPanelVisible', 'largePreview'],
+  editorFactory: Ember.inject.service(),
 
   itsSaved: Ember.computed("model.hasDirtyAttributes", function() {
     return (!this.get("model.hasDirtyAttributes"));
@@ -30,7 +31,17 @@ export default Ember.Controller.extend({
     if (e.metaKey) {
       if (e.which === 83 || e.which === 13) {
         e.preventDefault();
-        this.transitionToRoute("edit.previewModal", this.get('model'));
+
+        this.get('model').save().then(() => {
+          this.send('reload', this.get('model'));
+
+          setTimeout(() => {
+            this.get("editorFactory").tryToFocus();
+            console.log("focus!;");
+          }, 3000);
+        });
+
+        //this.transitionToRoute("edit.previewModal", this.get('model'));
       }
     }
   },
