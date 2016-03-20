d2eeee320eb76fb625037a5399f37eed3bc6f0e4
Hugo Ruscitti <hugoruscitti@gmail.com>
2016-03-20 15:19:58 -0300

    Agregando botón para crear proyectos nuevos.

--- app/controllers/project.js
+++ app/controllers/project.js
@@ -4,6 +4,20 @@ export default Ember.Controller.extend({
   actions: {
     remove(project) {
       project.destroyRecord();
+    },
+    newProject() {
+      let name = prompt("Indique el nombre del proyecto");
+
+      if (name) {
+        let record = this.store.createRecord("project", {
+          title: name,
+          code: "let pilas;\npilas.fondos.Plano();"
+        });
+
+        record.save().then((data) => {
+          this.transitionToRoute("edit", record);
+        });
+      }
     }
   }
 });
