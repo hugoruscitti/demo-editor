dd005379d663fd18177c64cc6c228285982294f1
Hugo Ruscitti <hugoruscitti@gmail.com>
2016-03-26 23:09:31 -0300

    Mejorando el selector de resolución.

--- app/controllers/edit/settings-modal.js
+++ app/controllers/edit/settings-modal.js
@@ -1,9 +1,22 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  resolution: "640x480",
+
   actions: {
-    toggleModal: function() {
+    toggleModal() {
       this.transitionToRoute('edit');
+    },
+    cancel() {
+      this.send("toggleModal");
+    },
+    confirm() {
+      let values = this.get("resolution").split("x");
+
+      this.set("model.ancho", parseInt(values[0], 10));
+      this.set("model.alto", parseInt(values[1], 10));
+
+      this.send("toggleModal");
     }
   }
 });
