e8dd5a2281e64d62f912c10918630974eae61dd0
Hugo Ruscitti <hugoruscitti@gmail.com>
2016-04-16 22:19:37 -0300

    Reparando la barra de status en ios.

--- app/routes/mobile/index.js
+++ app/routes/mobile/index.js
@@ -3,7 +3,8 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   actions: {
     visitExternalSite() {
-      navigator.app.loadUrl("http://google.com", {openExternal : true});
+      let url = "http://www.pilas-engine.com.ar";
+      window.open(url, '_system');
     }
   }
 });
