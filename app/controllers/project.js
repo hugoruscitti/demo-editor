333f9b0e65b2550572db922365cdf59aefe4fdb9
Hugo Ruscitti <hugoruscitti@gmail.com>
2016-04-21 14:41:47 -0300

    Implementando sweet-alert para evitar usar prompt en electron closes #11 closes #16

--- app/controllers/project.js
+++ app/controllers/project.js
@@ -1,23 +1,35 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  sweetalert: Ember.inject.service(),
   actions: {
     remove(project) {
       project.destroyRecord();
     },
     newProject() {
-      let name = prompt("Indique el nombre del proyecto");
 
-      if (name) {
+      let options = {
+                      title: "Creando un proyecto",
+                      text: "Ingresá un título para el proyecto:",
+                      type: "input",
+                      inputPlaceholder: this.get("title"),
+                      cancelButtonText: "Cancelar",
+                      confirmButtonText: "Crear proyecto",
+                      showCancelButton: true};
+
+      this.get("sweetalert").prompt(options).then((name) => {
         let record = this.store.createRecord("project", {
           title: name,
-          code: "let pilas;\npilas.fondos.Plano();"
+          ancho: 640,
+          alto: 480,
+          code: "let pilas;\npilas.fondos.plano();"
         });
 
         record.save().then(() => {
           this.transitionToRoute("edit", record);
         });
-      }
+      });
+
     }
   }
 });
