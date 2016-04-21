8785af6e53c553c310ad038d9419a8ba818fe95e
Hugo Ruscitti <hugoruscitti@gmail.com>
2016-04-21 15:48:22 -0300

    Implementando dialogo de pregunta para evitar que se salga sin guardar los cambios refs #16

--- app/routes/edit.js
+++ app/routes/edit.js
@@ -1,6 +1,8 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  sweetalert: Ember.inject.service(),
+
   activate() {
     this.controllerFor('edit').enableShortcuts();
   },
@@ -11,19 +13,45 @@ export default Ember.Route.extend({
 
   actions: {
     willTransition(transition) {
-      var message = "¿Estás seguro de abandonar sin guardar?";
+
+      var message = "¿Vas a abandonar sin guardar el proyecto?";
       var hasUnsavedChanges = ! this.controller.get('itsSaved');
 
-      if (hasUnsavedChanges && (transition.targetName === "project.index" || transition.targetName === "index.index" )) {
-        var hasConfirm = confirm(message);
+      if (!this.get('allowTransition')) {
+        transition.abort();
 
-        if (!hasConfirm) {
-          transition.abort();
+        if (hasUnsavedChanges && (transition.targetName === "project.index" || transition.targetName === "index.index" )) {
+          this.get("sweetalert").confirm({
+            title: "mmmm...",
+            text: message,
+            type: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#DD6B55",
+            confirmButtonText: "Si, abandono",
+            cancelButtonText: "No, quiero volver al editor!"
+          }).then((hasConfirm) => {
+
+            if (!hasConfirm) {
+              transition.abort();
+            } else {
+
+              Ember.run.later(() => {
+                this.controller.discartModelChanges();
+                this.set('allowTransition', true);
+                transition.retry();
+              }, 10);
+
+            }
+          });
         } else {
-          this.controller.discartModelChanges();
-          return true;
+          Ember.run.later(() => {
+            this.controller.discartModelChanges();
+            this.set('allowTransition', true);
+            transition.retry();
+          }, 10);
         }
       } else {
+        this.set('allowTransition', false);
         return true;
       }
     }
--- app/services/sweetalert.js
+++ app/services/sweetalert.js
@@ -1,6 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Service.extend({
+  confirm(options) {
+    return new Ember.RSVP.Promise((resolve) => {
+      swal(options, function(isConfirm) {
+        resolve(isConfirm);
+      });
+    });
+  },
   prompt(options) {
     return new Ember.RSVP.Promise((resolve, reject) => {
       swal(options, function(inputValue) {
