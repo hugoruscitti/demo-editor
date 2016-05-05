04c0f46c2f245a417cb2ce66a45e129774923baa
Hugo Ruscitti <hugoruscitti@gmail.com>
2016-05-05 16:24:48 -0300

    update

--- app/controllers/test.js
+++ app/controllers/test.js
@@ -1,4 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  actions: {
+    didLoad(iframe) {
+      console.log("Ha cargado, y con el argumento ", iframe);
+      window['ifr'] = iframe;
+    }
+  }
 });
